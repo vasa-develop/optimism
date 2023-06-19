ebbe0003bb54cfb501c24129bbeb193915582dfa
protolambda <proto@protolambda.com>
2023-06-20 00:45:34 +0200

    devnet: split L1/L2 geth entrypoint, set faster recommit time

--- ops-bedrock/op-entrypoint.sh
+++ ops-bedrock/op-entrypoint.sh
@@ -4,25 +4,10 @@ set -exu
 VERBOSITY=${GETH_VERBOSITY:-3}
 GETH_DATA_DIR=/db
 GETH_CHAINDATA_DIR="$GETH_DATA_DIR/geth/chaindata"
-GETH_KEYSTORE_DIR="$GETH_DATA_DIR/keystore"
 GENESIS_FILE_PATH="${GENESIS_FILE_PATH:-/genesis.json}"
 CHAIN_ID=$(cat "$GENESIS_FILE_PATH" | jq -r .config.chainId)
-BLOCK_SIGNER_PRIVATE_KEY="3e4bde571b86929bf08e2aaad9a6a1882664cd5e65b96fff7d03e1c4e6dfa15c"
-BLOCK_SIGNER_ADDRESS="0xca062b0fd91172d89bcd4bb084ac4e21972cc467"
 RPC_PORT="${RPC_PORT:-8545}"
 WS_PORT="${WS_PORT:-8546}"
-
-if [ ! -d "$GETH_KEYSTORE_DIR" ]; then
-	echo "$GETH_KEYSTORE_DIR missing, running account import"
-	echo -n "pwd" > "$GETH_DATA_DIR"/password
-	echo -n "$BLOCK_SIGNER_PRIVATE_KEY" | sed 's/0x//' > "$GETH_DATA_DIR"/block-signer-key
-	geth account import \
-		--datadir="$GETH_DATA_DIR" \
-		--password="$GETH_DATA_DIR"/password \
-		"$GETH_DATA_DIR"/block-signer-key
-else
-	echo "$GETH_KEYSTORE_DIR exists."
-fi
 
 if [ ! -d "$GETH_CHAINDATA_DIR" ]; then
 	echo "$GETH_CHAINDATA_DIR missing, running init"
@@ -36,8 +21,6 @@ fi
 
 # Warning: Archive mode is required, otherwise old trie nodes will be
 # pruned within minutes of starting the devnet.
-
-# recommit = 1s, since L1 geth doesn't support lower recommit interval
 
 exec geth \
 	--datadir="$GETH_DATA_DIR" \
@@ -57,17 +40,13 @@ exec geth \
 	--nodiscover \
 	--maxpeers=0 \
 	--networkid=$CHAIN_ID \
-	--unlock=$BLOCK_SIGNER_ADDRESS \
-	--mine \
-	--miner.etherbase=$BLOCK_SIGNER_ADDRESS \
-	--password="$GETH_DATA_DIR"/password \
-	--allow-insecure-unlock \
 	--authrpc.addr="0.0.0.0" \
 	--authrpc.port="8551" \
 	--authrpc.vhosts="*" \
+	--authrpc.jwtsecret=/config/test-jwt-secret.txt \
 	--gcmode=archive \
 	--metrics \
 	--metrics.addr=0.0.0.0 \
 	--metrics.port=6060 \
-	--miner.recommit=1s \
+	--miner.recommit=100ms \
 	"$@"
